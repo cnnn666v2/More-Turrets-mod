96ff178847af0fad79c057f0b1781bf7cb91dc97
Cnnn666 <83084283+cnnn666v2@users.noreply.github.com>
2024-03-10 19:33:21 +0100

    umieram

--- src/cnnn666/MoreTurrets.java
+++ src/cnnn666/MoreTurrets.java
@@ -1,4 +1,4 @@
-package example;
+package cnnn666;
 
 import arc.*;
 import arc.util.*;
@@ -9,9 +9,9 @@ import mindustry.gen.*;
 import mindustry.mod.*;
 import mindustry.ui.dialogs.*;
 
-public class ExampleJavaMod extends Mod{
+public class MoreTurrets extends Mod{
 
-    public ExampleJavaMod(){
+    public MoreTurrets(){
         Log.info("Loaded ExampleJavaMod constructor.");
 
         //listen for game load event
@@ -21,7 +21,7 @@ public class ExampleJavaMod extends Mod{
                 BaseDialog dialog = new BaseDialog("frog");
                 dialog.cont.add("behold").row();
                 //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-                dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
+                dialog.cont.image(Core.atlas.find("more-turrets-cnnn666-frog")).pad(20f).row();
                 dialog.cont.button("I see", dialog::hide).size(100f, 50f);
                 dialog.show();
             });
